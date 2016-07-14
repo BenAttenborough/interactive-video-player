06b24c3d008505b7a6c13b1c1fe17e2ef2a4647a
Ben Attenborough <battenborough@gmail.com>
2016-07-14 16:46:31 +0100

    Work on build video controls

--- js/video.js
+++ js/video.js
@@ -3,3 +3,23 @@
  */
 
 console.log("video.js is working");
+
+if (!!document.createElement('video').canPlayType) {
+    console.log("HTML5 video supported");
+
+    var videoContainer = $( "#video-container" );
+    var videoSource = $( "#video-source" );
+    var playButton = $( "button#play" );
+
+    //Bug - the page is already loaded so controls will still display
+    videoSource.controls = false;
+
+    console.log(videoSource.controls);
+
+    playButton.on( "click", function( event ) {
+        console.log("Play button pressed");
+    });
+    videoSource.on( "click", function( event ) {
+        console.log("Video Source clicked");
+    });
+}
