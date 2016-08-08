58c2eacacde139780a43af90e52f43fa230e1df0
Ben Attenborough <battenborough@gmail.com>
2016-08-08 22:12:25 +0100

    Work on responsiveness

--- js/video.js
+++ js/video.js
@@ -42,7 +42,7 @@ VideoPlayer.prototype.constructInterface = function () {
     progBarNode.className = "progContainer__bar";
 
     var buttonsNode = document.createElement("div");
-    buttonsNode.className = "buttons";
+    buttonsNode.className = "buttons clearfix";
 
     var timeNode = document.createElement("div");
     timeNode.className = "buttons__timeContainer";
