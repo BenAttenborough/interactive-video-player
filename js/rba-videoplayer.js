746b5003b8e22c97c4ff1d010d9a7739014546f0
Ben Attenborough <battenborough@gmail.com>
2016-08-29 16:21:42 +0100

    Implementing event binding function

--- js/rba-videoplayer.js
+++ js/rba-videoplayer.js
@@ -7,6 +7,7 @@ var RBA_Videoplayer = RBA_Videoplayer || {};
 
 var Video = function () {
     this.source = document.getElementById("video_source");
+    this.buttonPlay = document.getElementById("buttons_play");
 
     this.init();
 };
@@ -14,10 +15,23 @@ var Video = function () {
 Video.prototype.init = function () {
     console.log("Video player started");
     this.source.controls = false;
+    this.setButtonEvents();
+};
+
+Video.prototype.setButtonEvents = function () {
+    this.addListener(this.buttonPlay, "click", this.playPauseVideo);
 };
 
 Video.prototype.play = function () {
     this.source.play();
+};
+
+Video.prototype.playPauseVideo = function () {
+    this.play();
+};
+
+Video.prototype.addListener = function (element, type, func) {
+    element.addEventListener(type, func.bind(this));
 };
 
 RBA_Videoplayer.video = new Video();
