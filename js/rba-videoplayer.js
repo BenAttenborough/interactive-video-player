7b57837477281ac42acf0e6b082a94f03b72b967
Ben Attenborough <battenborough@gmail.com>
2016-08-29 15:35:56 +0100

    Adding controls

--- js/rba-videoplayer.js
+++ js/rba-videoplayer.js
@@ -2,20 +2,22 @@
  * Created by ben on 29/08/2016.
  */
 
-
-
 // global namespace
 var RBA_Videoplayer = RBA_Videoplayer || {};
 
 var Video = function () {
-    var author = "Ben Attenborough";
-
     this.source = document.getElementById("video_source");
 
+    this.init();
+};
+
+Video.prototype.init = function () {
     console.log("Video player started");
-    this.showAuthor = function() {
-        console.log(author);
-    }
+    this.source.controls = false;
+};
+
+Video.prototype.play = function () {
+    this.source.play();
 };
 
 RBA_Videoplayer.video = new Video();
