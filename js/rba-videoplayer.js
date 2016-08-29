f3fe9f98f0f36ddf7cd649f6ba88a8f09cfb7353
Ben Attenborough <battenborough@gmail.com>
2016-08-29 17:40:20 +0100

    Work on progress and buffering bars

--- js/rba-videoplayer.js
+++ js/rba-videoplayer.js
@@ -9,6 +9,8 @@ var Video = function () {
     this.source = document.getElementById("video_source");
     this.buttonPlay = document.getElementById("buttons_play");
     this.buttonPlayIcon = document.getElementById("icon_play");
+    this.progressBar = document.getElementById("progressBar_prog");
+    this.buffBar = document.getElementById("progressBar_buff");
 
     this.init();
 };
@@ -17,10 +19,15 @@ Video.prototype.init = function () {
     console.log("Video player started");
     this.source.controls = false;
     this.setButtonEvents();
+    this.setTimingEvents();
 };
 
 Video.prototype.setButtonEvents = function () {
     this.addListener(this.buttonPlay, "click", this.playPauseVideo);
+};
+
+Video.prototype.setTimingEvents = function () {
+    this.addListener(this.source, "timeupdate", this.updateVideoStatus);
 };
 
 Video.prototype.play = function () {
@@ -35,7 +42,25 @@ Video.prototype.playPauseVideo = function () {
     else {
         this.source.pause();
         this.buttonPlayIcon.src = "assets/icons/play-icon.png";
-    }};
+    }
+};
+
+Video.prototype.updateVideoStatus = function () {
+    this.updateProgressBar();
+    this.updateBuffBar();
+};
+
+Video.prototype.updateProgressBar = function () {
+    var percentComplete = Math.round(( this.source.currentTime / this.source.duration ) * 100) + "%";
+    this.progressBar.setAttribute("style", "Width: " + percentComplete);
+};
+
+Video.prototype.updateBuffBar = function () {
+    var buffered = this.source.buffered;
+    var bufferedEnd = buffered.end(buffered.length - 1);
+    var percentComplete = Math.round(( bufferedEnd / this.source.duration ) * 100) + "%";
+    this.buffBar.setAttribute("style", "Width: " + percentComplete);
+};
 
 Video.prototype.addListener = function (element, type, func) {
     element.addEventListener(type, func.bind(this));
