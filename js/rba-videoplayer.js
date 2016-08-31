02bb97b13db55d9b927f1ad9614162827db34c10
Ben Attenborough <battenborough@gmail.com>
2016-08-31 23:48:09 +0100

    Volume control changes
    Volume bar now set to zero when mute pressed, returns to original value
    if unmated

--- js/rba-videoplayer.js
+++ js/rba-videoplayer.js
@@ -127,6 +127,13 @@ Video.prototype.muteVideo = function () {
     var volOffImg = 'assets/icons/volume-off-icon.png';
     this.source.muted = !this.source.muted;
     this.toggleButton(this.source.muted, this.buttonMuteIcon, volOnImg, volOffImg);
+    console.log(this.source.muted);
+    if (this.source.muted) {
+        this.gainBarLevel.setAttribute("style", "height: 100%");
+    } else {
+        var percentVolume = Math.round(this.source.volume * 100) + "%";
+        this.gainBarLevel.setAttribute("style", "height: " + percentVolume);
+    }
 };
 
 Video.prototype.toggleButton = function (button, element, onIcon, offIcon) {
